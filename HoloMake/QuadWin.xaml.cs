51312c2f4facbcd77397b4ec2607136c921867cd
Gabriel Ripardo <gts.senna@gmail.com>
2018-08-31 15:53:13 -0300

    Software com as funcionalidades básicas funcionando.
    A falta sincronização dos quatro vídeos foi resolvida. 
    A rotação dos vídeos foi implementada.
    Controle de execução dos vídeos está sendo desenvolvida.

---
+++ HoloMake/QuadWin.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HoloMake
+{
+    /// <summary>
+    /// Lógica interna para QuadWin.xaml
+    /// </summary>
+    public partial class QuadWin : Window
+    {
+        public QuadWin(VisualBrush visualBrush)
+        {
+            InitializeComponent();
+
+            displayTop.Fill = visualBrush;
+            displayLeft.Fill = visualBrush;
+            displayRight.Fill = visualBrush;
+            displayBottom.Fill = visualBrush;
+        }
+
+        public QuadWin(MediaElement videoM)
+        {
+            InitializeComponent();
+
+            displayTop.Fill = new VisualBrush(videoM);
+            displayLeft.Fill = new VisualBrush(videoM);
+            displayRight.Fill = new VisualBrush(videoM);
+            displayBottom.Fill = new VisualBrush(videoM);
+        }
+
+        private void btnMenosW_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnMaisW_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnMaisH_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnMenosH_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Canvas_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+
+        }
+    }
+}
